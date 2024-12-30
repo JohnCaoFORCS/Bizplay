d7dc3eb636b813e484ed3b4b0008b0e177ef5ff9
Cao Viet Huy <caoviethuy138@forcs.com>
2024-12-30 13:08:50 +0700

    refactor: logic filter sort table

--- components/tables/mock-data/receipt-table-data.ts
+++ components/tables/mock-data/receipt-table-data.ts
@@ -1,61 +1,50 @@
 import { Task } from "./utils";
 
-function generateRandomString(length = 10) {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
 export const receiptTableData: Task[] = [
   {
-    id: generateRandomString(),
+    id: "A1B2C3D4E5",
     userId: "1",
     createdAt: "2024-01-01",
     cashAmount: 100000,
   },
   {
-    id: generateRandomString(),
+    id: "F6G7H8I9J0",
     userId: "2",
     createdAt: "2024-02-28",
     cashAmount: 200000,
   },
   {
-    id: generateRandomString(),
+    id: "K1L2M3N4O5",
     userId: "3",
     createdAt: "2024-04-30",
     cashAmount: 300000,
   },
   {
-    id: generateRandomString(),
+    id: "P6Q7R8S9T0",
     userId: "4",
     createdAt: "2024-02-01",
     cashAmount: 400000,
   },
   {
-    id: generateRandomString(),
+    id: "U1V2W3X4Y5",
     userId: "5",
     createdAt: "2024-04-21",
     cashAmount: 500000,
   },
   {
-    id: generateRandomString(),
+    id: "Z6A7B8C9D0",
     userId: "6",
     createdAt: "2024-11-01",
     cashAmount: 600000,
   },
   {
-    id: generateRandomString(),
+    id: "E1F2G3H4I5",
     userId: "7",
     createdAt: "2024-12-29",
     cashAmount: 600000,
   },
   {
-    id: generateRandomString(),
+    id: "J6K7L8M9N0",
     userId: "8",
     createdAt: "2024-07-04",
     cashAmount: 700000,
