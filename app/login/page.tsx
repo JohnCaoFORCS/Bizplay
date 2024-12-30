46a71550fc28fcf58e39e0ccc03d8b100fbed7aa
JohnCaoFORCS <caoviethuy138@forcs.com>
2024-12-30 09:45:08 +0700

    Login Screen (#1)
    * refactor: add login screen
    
    * refactor: install components

---
+++ app/login/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const accounts = [
+    {
+      email: "admin@forcs.com",
+      password: "admin",
+    },
+  ];
+
+  const handleLogin = () => {
+    const account = accounts.find((account) => account.email === email);
+    if (account && account.password === password) {
+      router.push("/dashboard");
+      return;
+    }
+
+    alert("Invalid email or password");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 w-screen h-screen">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button type="submit" className="w-full" onClick={handleLogin}>
+                Login
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
