9a76068214fdf0fc96641d02816f1ea69f8c501a
John Cao <caoviethuy138@forcs.com>
2025-01-03 15:51:11 +0700

    refactor: optimize UI

--- components/views/ViewReSubmitResolution.tsx
+++ components/views/ViewReSubmitResolution.tsx
@@ -8,11 +8,11 @@ import { toast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { getStatusBadge, Resolution } from "../tables/mock-data/utils";
@@ -20,197 +20,216 @@ import { CircleX } from "lucide-react";
 import MenuApprovers from "../menus/MenuApprovers";
 
 const receiptFormSchema = z.object({
-  id: z.string(),
-  userId: z.string(),
-  createdAt: z.string(),
-  cashAmount: z.number(),
-  submitter: z.string(),
-  submitDate: z.string(),
-  title: z.string(),
-  content: z.string(),
-  note: z.string(),
-  category: z.string(),
-  description: z.string(),
-  applicationAmount: z.number(),
-  attachFile: z.string().optional(),
-  selectApprover: z.string(),
+    id: z.string(),
+    userId: z.string(),
+    createdAt: z.string(),
+    cashAmount: z.number(),
+    submitter: z.string(),
+    submitDate: z.string(),
+    title: z.string(),
+    content: z.string(),
+    note: z.string(),
+    category: z.string(),
+    description: z.string(),
+    applicationAmount: z.number(),
+    attachFile: z.string().optional(),
+    selectApprover: z.string(),
 });
 
 type ReceiptFormValues = z.infer<typeof receiptFormSchema>;
 
 // This can come from your database or API.
 const defaultValues: Partial<ReceiptFormValues> = {
-  id: "",
-  userId: "",
-  createdAt: "",
-  cashAmount: 0,
-  submitter: "",
-  submitDate: new Date().toISOString().split("T")[0],
-  title: "",
-  content: "",
-  note: "",
-  category: "",
-  description: "",
-  applicationAmount: 0,
-  attachFile: "",
-  selectApprover: "",
+    id: "",
+    userId: "",
+    createdAt: "",
+    cashAmount: 0,
+    submitter: "",
+    submitDate: new Date().toISOString().split("T")[0],
+    title: "",
+    content: "",
+    note: "",
+    category: "",
+    description: "",
+    applicationAmount: 0,
+    attachFile: "",
+    selectApprover: "",
 };
 
 type ViewReSubmitResolutionProps = {
-  resolution: Resolution;
-  setSelectedReSubmitResolution: (resolution: Resolution | undefined) => void;
+    resolution: Resolution;
+    setSelectedReSubmitResolution: (resolution: Resolution | undefined) => void;
 };
 
 export function ViewReSubmitResolution({
-  resolution,
-  setSelectedReSubmitResolution,
+    resolution,
+    setSelectedReSubmitResolution,
 }: ViewReSubmitResolutionProps) {
-  const form = useForm<ReceiptFormValues>({
-    resolver: zodResolver(receiptFormSchema),
-    defaultValues,
-    mode: "onChange",
-  });
-
-  function onSubmit(data: ReceiptFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
+    const form = useForm<ReceiptFormValues>({
+        resolver: zodResolver(receiptFormSchema),
+        defaultValues,
+        mode: "onChange",
     });
-  }
 
-  return (
-    <div className="flex flex-col items-center w-full h-full gap-4 px-10 py-4">
-      <div className="flex w-full items-center justify-between">
-        {getStatusBadge(
-          resolution.status,
-          "flex items-center justify-center w-[100px] h-8 text-sm"
-        )}
-        <div className="flex items-center gap-2">
-          <Button
-            onClick={() => {
-              setSelectedReSubmitResolution(undefined);
-            }}
-            variant={"outline"}
-            size={"default"}
-          >
-            Back
-          </Button>
-          <Button variant={"outline"} size={"default"}>
-            Change Approver
-          </Button>
-          <Button variant={"default"} size={"default"}>
-            Re-submit
-          </Button>
-        </div>
-      </div>
-      <div className="flex flex-col w-full gap-2">
-        <span className="text-lg text-center font-semibold">
-          Expense Resolution
-        </span>
-        <div className="flex flex-col w-full">
-          <FieldResolution label="Submitter" rightChild={<Input />} />
-          <FieldResolution
-            label="Submit Date"
-            rightChild={
-              <Input
-                type="datetime"
-                value={new Date().toISOString().split("T")[0]}
-                readOnly
-              />
-            }
-          />
-          <FieldResolution label="Title" rightChild={<Input />} />
-          <FieldResolution label="Content" rightChild={<Textarea />} />
-          <FieldResolution
-            label="Note"
-            rightChild={<Textarea />}
-            isLast={true}
-          />
-        </div>
-      </div>
+    function onSubmit(data: ReceiptFormValues) {
+        toast({
+            title: "You submitted the following values:",
+            description: (
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">
+                        {JSON.stringify(data, null, 2)}
+                    </code>
+                </pre>
+            ),
+        });
+    }
 
-      <div className="flex flex-col w-full">
-        <span className="text-base font-semibold">
-          Total ￦325,500 of 2 Expenses (Application Amount ￦325,500){" "}
-        </span>
-        <div className="w-full h-[2px] bg-primary mb-4" />
-        <div className="flex justify-between">
-          <div className="flex items-center justify-between h-fit p-4 gap-8 border-2 border-primary">
-            <div className="flex flex-col gap-1">
-              <span>2024-11-30 20:30</span>
-              <span className="font-semibold">Randy's Pub</span>
-              <span>Visa 1234-1234</span>
+    return (
+        <div className="flex flex-col items-center w-full h-full gap-4 px-10 py-4">
+            <div className="flex w-full items-center justify-between">
+                {getStatusBadge(
+                    resolution.status,
+                    "flex items-center justify-center w-[100px] h-8 text-sm"
+                )}
+                <div className="flex items-center gap-2">
+                    <Button
+                        onClick={() => {
+                            setSelectedReSubmitResolution(undefined);
+                        }}
+                        variant={"outline"}
+                        size={"default"}
+                    >
+                        Back
+                    </Button>
+                    <Button variant={"outline"} size={"default"}>
+                        Change Approver
+                    </Button>
+                    <Button variant={"default"} size={"default"}>
+                        Re-submit
+                    </Button>
+                </div>
             </div>
-            <span className="font-semibold text-primary">￦325,500</span>
-          </div>
-          <div className="flex flex-col gap-2 w-1/2">
-            <div className="flex items-center gap-1">
-              <span className="w-1/4 flex justify-end pr-2">Category</span>
-              <Select>
-                <SelectTrigger className="w-3/4">
-                  <SelectValue placeholder="Select Category" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">Category 1</SelectItem>
-                  <SelectItem value="2">Category 2</SelectItem>
-                  <SelectItem value="3">Category 3</SelectItem>
-                </SelectContent>
-              </Select>
+            <div className="flex flex-col w-full gap-2">
+                <span className="text-lg text-center font-semibold">
+                    Expense Resolution
+                </span>
+                <div className="flex flex-col w-full">
+                    <FieldResolution label="Submitter" rightChild={<Input />} />
+                    <FieldResolution
+                        label="Submit Date"
+                        rightChild={
+                            <Input
+                                type="datetime"
+                                value={new Date().toISOString().split("T")[0]}
+                                readOnly
+                            />
+                        }
+                    />
+                    <FieldResolution label="Title" rightChild={<Input />} />
+                    <FieldResolution
+                        label="Content"
+                        rightChild={<Textarea />}
+                    />
+                    <FieldResolution
+                        label="Note"
+                        rightChild={<Textarea />}
+                        isLast={true}
+                    />
+                </div>
             </div>
-            <div className="flex gap-1">
-              <span className="w-1/4 flex justify-end pr-2">Description</span>
-              <Textarea className="w-3/4" />
-            </div>
-            <div className="flex gap-1">
-              <span className="w-1/4 flex justify-end pr-2">Amount</span>
-              <Input className="w-3/4" />
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="w-full h-[2px] bg-primary mt-2" />
-      <div className="flex flex-col gap-1 items-start w-full">
-        <Button className="w-1/4" variant={"outline"} size={"lg"}>
-          Attach File
-        </Button>
-        <div className="flex items-center gap-2">
-          <span>Subcontract_agreement_Microsoft.pdf</span>
-          <CircleX className="text-gray-400 w-5 h-5 hover:text-primary cursor-pointer" />
+            <div className="flex flex-col w-full">
+                <span className="text-base font-semibold mt-12">
+                    Total ￦325,500 of 2 Expenses (Application Amount ￦325,500){" "}
+                </span>
+                <div className="w-full h-[2px] bg-primary mb-4" />
+                <div className="flex justify-between">
+                    <div className="flex items-center justify-between h-fit p-4 gap-8 border-2 border-primary">
+                        <div className="flex flex-col gap-1">
+                            <span>2024-11-30 20:30</span>
+                            <span className="font-semibold">Randy's Pub</span>
+                            <span>Visa 1234-1234</span>
+                        </div>
+                        <span className="font-semibold text-primary">
+                            ￦325,500
+                        </span>
+                    </div>
+                    <div className="flex flex-col gap-2 w-1/2">
+                        <div className="flex items-center gap-1">
+                            <span className="w-1/4 flex justify-end pr-2">
+                                Category
+                            </span>
+                            <Select>
+                                <SelectTrigger className="w-3/4">
+                                    <SelectValue placeholder="Select Category" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="1">
+                                        Category 1
+                                    </SelectItem>
+                                    <SelectItem value="2">
+                                        Category 2
+                                    </SelectItem>
+                                    <SelectItem value="3">
+                                        Category 3
+                                    </SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div className="flex gap-1">
+                            <span className="w-1/4 flex justify-end pr-2">
+                                Description
+                            </span>
+                            <Textarea className="w-3/4" />
+                        </div>
+                        <div className="flex gap-1">
+                            <span className="w-1/4 flex justify-end pr-2">
+                                Amount
+                            </span>
+                            <Input className="w-3/4" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full h-[2px] bg-primary mt-12" />
+            <div className="flex flex-col gap-1 items-start w-full">
+                <Button className="w-1/4" variant={"outline"} size={"lg"}>
+                    Attach File
+                </Button>
+                <div className="flex items-center gap-2">
+                    <span>Subcontract_agreement_Microsoft.pdf</span>
+                    <CircleX className="text-gray-400 w-5 h-5 hover:text-primary cursor-pointer" />
+                </div>
+            </div>
+            <span className="flex items-center justify-start w-full text-base font-semibold -mb-4 mt-12">
+                Approvers
+            </span>
+            <div className="w-full h-[2px] bg-primary mb-2" />
+            <div className="w-full -mt-2">
+                <MenuApprovers />
+            </div>
         </div>
-      </div>
-      <span className="flex items-center justify-start w-full text-base font-semibold -mb-4 mt-1">
-        Approvers
-      </span>
-      <div className="w-full h-[2px] bg-primary mb-2" />
-      <div className="w-full -mt-2">
-        <MenuApprovers />
-      </div>
-    </div>
-  );
+    );
 }
 
 type FieldResolutionProps = {
-  label: string;
-  rightChild: React.ReactNode;
-  isLast?: boolean;
+    label: string;
+    rightChild: React.ReactNode;
+    isLast?: boolean;
 };
 
 function FieldResolution({ label, rightChild, isLast }: FieldResolutionProps) {
-  return (
-    <div
-      className={`flex w-full border-2 ${
-        isLast ? "border-b-2" : "border-b-0"
-      } border-gray-200`}
-    >
-      <div className="w-1/5 pl-8 flex items-center border-r-2 border-gray-200">
-        {label}
-      </div>
-      <div className="w-4/5 p-2 flex items-center">{rightChild}</div>
-    </div>
-  );
+    return (
+        <div
+            className={`flex w-full border-2 ${
+                isLast ? "border-b-2" : "border-b-0"
+            } border-gray-200`}
+        >
+            <div className="w-1/5 pl-8 flex items-center border-r-2 border-gray-200">
+                {label}
+            </div>
+            <div className="w-4/5 p-2 flex items-center">{rightChild}</div>
+        </div>
+    );
 }
